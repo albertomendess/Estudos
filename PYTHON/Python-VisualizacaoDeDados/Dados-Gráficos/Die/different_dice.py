d547841d5e964c8bbf21c78da891a995b0e2be33
Alberto-Mendess <78062732+Alberto-Mendess@users.noreply.github.com>
2021-03-06 15:48:37 -0300

    Aprendendo a analisar um histograma
    Histograma que mostra o lançamento de um dado de 6 lados e um dado de 10 lados 50.000 vezes.

---
+++ PYTHON/Python-VisualizacaoDeDados/Dados-Gráficos/Die/different_dice.py
@@ -0,0 +1,35 @@
+from die import Die
+
+import pygal
+
+
+# Cria um D6 e um D10
+die_1 = Die()
+die_2 = Die(10)
+
+
+# Faz alguns lançamentos e armazena os resultados em uma lista
+results = []
+for roll_num in range(50000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+
+# Analisa os resultados
+frequecies = []
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result+1):
+    frequency = results.count(value)
+    frequecies.append(frequency)
+
+
+# Visualiza os resultados
+hist = pygal.Bar()
+
+hist.title = "Results of rolling a D6 and a D10 50,000 times."
+hist.x_labels = list(range(2, 17))
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6 + D10', frequecies)
+hist.render_to_file('PYTHON/Python-VisualizacaoDeDados/Dados-Gráficos/Die/different_dice_visual.svg')
