8723672eddae3559e5a02879eacd5ea743517f01
Alberto-Mendess <78062732+Alberto-Mendess@users.noreply.github.com>
2021-03-09 16:10:21 -0300

    Update amusement_park.py

--- PYTHON/Python-Estudos/amusement_park.py
+++ PYTHON/Python-Estudos/amusement_park.py
@@ -1,13 +1,11 @@
 # SINTAXE if-elif-else
-age = 12
+age = 19
 
 if age < 4:
     price = 0
-elif age < 18:
+elif age < 18 or age > 64:
     price = 5
 elif age < 65:
     price = 10
-elif age >= 65: # podemos omitir o a instrução 'else' por uma 'elif' para ficar mais conciso
-    price = 5
 
 print("Your admission cost in $" + str(price) + ".")
