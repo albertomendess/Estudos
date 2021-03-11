c3563510c1c20e691148776bcb804e91640b3b9c
Alberto-Mendess <78062732+Alberto-Mendess@users.noreply.github.com>
2021-03-11 16:55:48 -0300

    Visualização gráfica no Death Valley em 2014
    Mostra a variação de temperatura máxima e mínima.

---
+++ PYTHON/Python-VisualizacaoDeDados/Dados-Gráficos/Dowload_de_dados/death_valley_2014.py
@@ -0,0 +1,41 @@
+import csv
+from datetime import datetime
+
+from matplotlib import pyplot as plt
+
+# Obtém as datas e as temperaturas máximas e mínimas do arquivo.
+filename = 'PYTHON/Python-VisualizacaoDeDados/Dados-Gráficos/Dowload_de_dados/death_valley_2014.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    dates, highs, lows = [], [], []
+    for row in reader:
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+
+
+# Faz plotagem dos dados.
+fig = plt.figure(dpi=100, figsize=(12, 8))
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+
+# Formata o gráfico.
+title = "Daily high and low temperatures - 2014\nDeath Valley, CA"
+plt.title(title, fontsize=20)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
