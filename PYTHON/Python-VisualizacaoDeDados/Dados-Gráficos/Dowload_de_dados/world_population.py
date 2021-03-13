dd202617eff533b78b5eeb1814b49ad86e31d9b6
Alberto-Mendess <78062732+Alberto-Mendess@users.noreply.github.com>
2021-03-13 14:28:48 -0300

    creating a complete a population map

--- PYTHON/Python-VisualizacaoDeDados/Dados-Gráficos/Dowload_de_dados/world_population.py
+++ PYTHON/Python-VisualizacaoDeDados/Dados-Gráficos/Dowload_de_dados/world_population.py
@@ -1,5 +1,6 @@
 import json
-
+import pygal
+from pygal_maps_world.maps import World
 from country_codes import get_country_code
 
 # Carrega os dados em uma lista
@@ -8,14 +9,18 @@ filename = 'Estudos/PYTHON/Python-VisualizacaoDeDados/Dados-Gráficos/Dowload_de
 with open(filename) as f:
     pop_data = json.load(f)
 
-# Exibe a população de cada país em 2010.
+# Constroi um dicinário com dados das populações.
+cc_populations = {}
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
-        country_name = pop_dict['Country Name']
+        country = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))
-        print(country_name + ": " + str(population))
-        code = get_country_code(country_name)
+        code = get_country_code(country)
         if code:
-            print(f"{code} : {str(population)}")
-        else:
-            print(f"ERROR - {country_name}")
+            cc_populations[code] = population
+
+w = World()
+w.title = "World population in 2010, by Country"
+w.add('2010', cc_populations)
+
+w.render_to_file('Estudos/PYTHON/Python-VisualizacaoDeDados/Dados-Gráficos/Dowload_de_dados/world_population.svg')
