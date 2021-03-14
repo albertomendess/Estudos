eca0f1f74086df06ece0407490a3e87f260aed43
Alberto-Mendess <78062732+Alberto-Mendess@users.noreply.github.com>
2021-03-14 17:25:00 -0300

    Update world_population.py

--- PYTHON/Python-VisualizacaoDeDados/Dados-Gráficos/Dowload_de_dados/world_population.py
+++ PYTHON/Python-VisualizacaoDeDados/Dados-Gráficos/Dowload_de_dados/world_population.py
@@ -3,13 +3,13 @@ import pygal
 from pygal_maps_world.maps import World
 from country_codes import get_country_code
 
-# Carrega os dados em uma lista
+# Carrega os dados em uma lista.
 filename = 'Estudos/PYTHON/Python-VisualizacaoDeDados/Dados-Gráficos/Dowload_de_dados/population_data.json'
 
 with open(filename) as f:
     pop_data = json.load(f)
 
-# Constroi um dicinário com dados das populações.
+# Constrói um dicionário com dados das populações.
 cc_populations = {}
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
