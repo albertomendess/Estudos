e47a0ee9fc9e33b5c787404ced9caef845325c8b
Alberto-Mendess <78062732+Alberto-Mendess@users.noreply.github.com>
2021-03-18 14:48:42 -0300

    Update country_codes.py

--- PYTHON/Python-VisualizacaoDeDados/Dados-Gráficos/Dowload_de_dados/country_codes.py
+++ PYTHON/Python-VisualizacaoDeDados/Dados-Gráficos/Dowload_de_dados/country_codes.py
@@ -3,31 +3,31 @@ from pygal_maps_world.i18n import COUNTRIES
 def get_country_code(country_name):
     """Devolve o código de duas letras da Pygal país, dado o seu nome."""
     for code, name in COUNTRIES.items():
-        if name == country_name:
+        if name == country_name.title():
             return code
-        elif country_name == 'Bolivia':
+        elif country_name.title() == 'Bolivia':
             return 'bo'
-        elif country_name == 'Congo, Rep.':
+        elif country_name.title() == 'Congo, Rep.':
             return 'cd'
-        elif country_name == 'Egypt, Arab Rep.':
+        elif country_name.title() == 'Egypt, Arab Rep.':
             return 'eg'
-        elif country_name == 'Gambia, The':
+        elif country_name.title() == 'Gambia, The':
             return 'gm'
-        elif country_name == 'Iran, Islamic Rep.':
+        elif country_name.title() == 'Iran, Islamic Rep.':
             return 'ir'
-        elif country_name == 'Korea, Rep.':
+        elif country_name.title() == 'Korea, Rep.':
             return 'kp'
-        elif country_name == 'Libya':
+        elif country_name.title() == 'Libya':
             return 'ly'
-        elif country_name == 'Slovak Republic':
+        elif country_name.title() == 'Slovak Republic':
             return 'sk'
-        elif country_name == 'Tanzania':
+        elif country_name.title() == 'Tanzania':
             return 'tz'
-        elif country_name == 'Venezuela, RB':
+        elif country_name.title() == 'Venezuela, RB':
             return 've'
-        elif country_name == 'Vietnam':
+        elif country_name.title() == 'Vietnam':
             return 'vt'
-        elif country_name == 'Yemen, Rep.':
+        elif country_name.title() == 'Yemen, Rep.':
             return 'ye'
     # Se o país não for encontrado, devolve None.
     return None
