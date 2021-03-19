224a2270ffdc8b94ddeae06a5de9480e085d35d9
Alberto-Mendess <78062732+Alberto-Mendess@users.noreply.github.com>
2021-03-19 06:39:44 -0300

    Update python_repos.py

--- PYTHON/Python-Estudos/python_repos.py
+++ PYTHON/Python-Estudos/python_repos.py
@@ -7,6 +7,18 @@ print("Status code:", r.status_code)
 
 # Armazena a resposta da API em uma variável.
 response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
 
-# Processa o resultado.
-print(response_dict.keys())
+# Explora informações sobre os repositórios.
+repo_dicts = response_dict['items']
+print("Repositories returned:", len(repo_dicts))
+
+print("\nSelected information about first repository:")
+for repo_dict in repo_dicts:
+    print('\nName:', repo_dict['name'])
+    print('Owner:', repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Created:', repo_dict['created_at'])
+    print('Updated:', repo_dict['updated_at'])
+    print('Description', repo_dict['description'])
