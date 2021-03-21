53d124e3dc08e31f2d3eae82c5c135619c7efa7b
Alberto-Mendess <78062732+Alberto-Mendess@users.noreply.github.com>
2021-03-21 16:13:05 -0300

    Update hn_graphics.py

--- PYTHON/Python-Estudos/hn_graphics.py
+++ PYTHON/Python-Estudos/hn_graphics.py
@@ -21,8 +21,6 @@ for submission_id in submission_ids[:30]:
         'title': response_dict['title'],
         'link': 'http://news.ycombinator.com/item?id=' + str (submission_id),
         'comments': response_dict.get('descendants', 0),
-        'xlink': ('https://hacker-news.firebaseio.com/v0/item/' + 
-            str(submission_id) + '.json'),
         }
     submission_dicts.append(submission_dict)
 
