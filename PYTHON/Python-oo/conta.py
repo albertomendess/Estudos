8328bcb3bbab77176f38760dd2853f74ffafed31
Alberto-Mendess <78062732+Alberto-Mendess@users.noreply.github.com>
2021-08-24 15:54:24 -0300

    Update conta.py

--- PYTHON/Python-oo/conta.py
+++ PYTHON/Python-oo/conta.py
@@ -2,22 +2,26 @@
 
 class Conta:
 
-
     def __init__(self, numero, titular, saldo, limite):
         print(f"Construindo objeto ... {self}")
-        self.numero  = numero
-        self.titular = titular
-        self.saldo   = saldo
-        self.limite  = limite
+        self.__numero  = numero
+        self.__titular = titular
+        self.__saldo   = saldo
+        self.__limite  = limite
 
 
     def extrato(self):
-        print(f"Saldo de $ {self.saldo} do titular {self.titular}")
+        print(f"Saldo de $ {self.__saldo} do titular {self.__titular}")
 
 
     def depositar(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
 
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
