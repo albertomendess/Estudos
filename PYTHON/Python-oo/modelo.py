020e9b714c9a27c3c6013bf60a3d085e77e06c6b
Alberto-Mendess <albertoemanuel40@gmail.com>
2021-11-10 17:19:04 -0300

    Update modelo.py

--- PYTHON/Python-oo/modelo.py
+++ PYTHON/Python-oo/modelo.py
@@ -1,21 +1,32 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome    = nome
+        self.nome    = nome.title()
         self.ano     = ano
         self.duracao = duracao
+        self.likes   = 0
+
+    def dar_like(self):
+        self.likes += 1
 
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome       = nome
+        self.nome       = nome.title()
         self.ano        = ano
         self.temporadas = temporadas
+        self.likes      = 0
 
+    def dar_like(self):
+        self.likes += 1
 
-vingadores = Filme('Vingadores - guerra infinita', 2018, 160)
-print(f"Nome: {vingadores.nome} - Ano: {vingadores.ano} "
-    f"- Duração: {vingadores.duracao}")
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.dar_like()
+print(f"Nome: {vingadores.nome} \nAno: {vingadores.ano}"
+    f"\nDuração: {vingadores.duracao} \nLikes: {vingadores.likes}\n")
 
-atlanta = Serie("Atlanta", 2018, 2)
-print(f"Nome: {atlanta.nome} - Ano: {atlanta.ano} "
-    f"- Temporadas: {atlanta.temporadas}")
+atlanta = Serie("atlanta", 2018, 2)
+atlanta.nome = "atlanda"
+atlanta.dar_like()
+atlanta.dar_like()
+print(f"Nome: {atlanta.nome} \nAno: {atlanta.ano} "
+    f"\nTemporadas: {atlanta.temporadas} \nLikes: {atlanta.likes}\n")
