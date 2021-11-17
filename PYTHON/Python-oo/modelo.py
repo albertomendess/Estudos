2e951f54d8290c23ae95ef7306171a4d8851f2f5
Alberto-Mendess <albertoemanuel40@gmail.com>
2021-11-17 18:45:12 -0300

    Update modelo.py

--- PYTHON/Python-oo/modelo.py
+++ PYTHON/Python-oo/modelo.py
@@ -1,23 +1,61 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome    = nome.title()
+        self.__likes = 0
+        self.__nome  = nome.title()
         self.ano     = ano
         self.duracao = duracao
-        self.likes   = 0
+
+
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self.__likes
 
     def dar_like(self):
-        self.likes += 1
+        self.__likes += 1
+
+
+
 
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome       = nome.title()
+        self.__likes    = 0
+        self.__nome     = nome.title()
         self.ano        = ano
         self.temporadas = temporadas
-        self.likes      = 0
+
+
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self.__likes
+
+
+
 
     def dar_like(self):
-        self.likes += 1
+        self.__likes += 1
+
+
+
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
@@ -25,7 +63,6 @@ print(f"Nome: {vingadores.nome} \nAno: {vingadores.ano}"
     f"\nDuração: {vingadores.duracao} \nLikes: {vingadores.likes}\n")
 
 atlanta = Serie("atlanta", 2018, 2)
-atlanta.nome = "atlanda"
 atlanta.dar_like()
 atlanta.dar_like()
 print(f"Nome: {atlanta.nome} \nAno: {atlanta.ano} "
